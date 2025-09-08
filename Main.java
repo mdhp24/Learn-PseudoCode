c05248550e4edfdeb2809548c864eb2551b59b32
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2025-09-08 21:24:50 +0700

    Learn implements PSEUDOCODE Day 2

--- Main.java
+++ Main.java
@@ -4,14 +4,26 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
-        // Program dari PSEUDOCODE menentukan sebuah bilangan adalah ganjil atau genap
-        int bilangan;
+        // // Program dari PSEUDOCODE menentukan sebuah bilangan adalah ganjil atau
+        // genap
+        // int bilangan;
 
-        bilangan = 99;
-        if (bilangan % 2 == 0) {
-            System.out.print(bilangan + " Bilangan genap");
-        } else {
-            System.out.print(bilangan + " Bilangan ganjil");
-        }
+        // bilangan = 99;
+        // if (bilangan % 2 == 0) {
+        // System.out.print(bilangan + " Bilangan genap");
+        // } else {
+        // System.out.print(bilangan + " Bilangan ganjil");
+        // }
+        // }
+
+        // Program dari PSEUDOCODE mencari luas lapangan sepak bola
+        int panjang, lebar, luas;
+
+        panjang = 100;
+        lebar = 64;
+        luas = panjang * lebar;
+
+        System.out.println("Luas lapangan sepak bola adalah " + luas);
+
     }
 }
