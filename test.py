5023de09f9bbc85c99a7a64782366ea97f430896
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2025-10-14 19:11:28 +0700

    Learn implements Python Day 1.1

---
+++ test.py
@@ -0,0 +1,11 @@
+# Menentukan nilai berdasarkan input pengguna
+
+nilai = int(input("Masukkan nilai: "))
+if nilai >= 80:
+    print("Nilai Anda A")
+elif nilai >= 70:
+    print("Nilai Anda B")
+elif nilai >= 60:
+    print("Nilai Anda C")
+else:
+    print("Nilai Anda D")
