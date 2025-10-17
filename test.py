ec7a68a05655e204a9562d09a595210fc4be8f43
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2025-10-17 12:54:21 +0700

    Learn implements Python Day 4.4

--- test.py
+++ test.py
@@ -256,7 +256,13 @@
 #     print(f"{tahun} bukan tahun kabisat")
 
 # Program untuk menghitung luas lingkaran
-import math
-radius = float(input("Masukkan jari-jari lingkaran: "))
-luas = math.pi * radius ** 2
-print(f"Luas lingkaran dengan jari-jari {radius} adalah {luas}")
+# import math
+# radius = float(input("Masukkan jari-jari lingkaran: "))
+# luas = math.pi * radius ** 2
+# print(f"Luas lingkaran dengan jari-jari {radius} adalah {luas}")
+
+# Program untuk menghitung luas segitiga
+alas = float(input("Masukkan alas segitiga: "))
+tinggi = float(input("Masukkan tinggi segitiga: "))
+luas = 0.5 * alas * tinggi
+print(f"Luas segitiga dengan alas {alas} dan tinggi {tinggi} adalah {luas}")
