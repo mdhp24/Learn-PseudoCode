33c0b968a7dec4eda3a4d0f54bea43d77ea55c34
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2025-10-22 16:12:08 +0700

    Learn implements Python Day 8.5

--- test.py
+++ test.py
@@ -554,8 +554,17 @@
 # print("Jumlah huruf konsonan dalam kalimat:", jumlah_konsonan)
 
 # Program untuk memeriksa apakah sebuah tahun adalah tahun kabisat
-tahun = int(input("Masukkan sebuah tahun: "))
-if (tahun % 4 == 0 and tahun % 100 != 0) or (tahun % 400 == 0):
-    print(f"{tahun} adalah tahun kabisat")
-else:
-    print(f"{tahun} bukan tahun kabisat")
+# tahun = int(input("Masukkan sebuah tahun: "))
+# if (tahun % 4 == 0 and tahun % 100 != 0) or (tahun % 400 == 0):
+#     print(f"{tahun} adalah tahun kabisat")
+# else:
+#     print(f"{tahun} bukan tahun kabisat")
+
+# Program untuk menghitung faktorial dari sebuah angka
+def faktorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * faktorial(n - 1)
+angka = int(input("Masukkan sebuah angka: "))
+print(f"Faktorial dari {angka} adalah {faktorial(angka)}")
