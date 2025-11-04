c8cdc3d4388afc46927c8c9feae3702a732160ff
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2025-11-04 17:31:50 +0700

    Learn implements Python Day 19.2

--- test.py
+++ test.py
@@ -1533,26 +1533,39 @@
 #         print("Bot: Aku belum paham maksudmu.")
 
 # sistem antrian toko sederhana
-from collections import deque
-antrian = deque()
-while True:
-    print("\n1. Tambah Antrian\n2. Layani Antrian\n3. Lihat Antrian\n4. Keluar")
-    pilih = input("Pilih menu: ")
+# from collections import deque
+# antrian = deque()
+# while True:
+#     print("\n1. Tambah Antrian\n2. Layani Antrian\n3. Lihat Antrian\n4. Keluar")
+#     pilih = input("Pilih menu: ")
 
-    if pilih == "1":
-        nama = input("Masukkan nama pelanggan: ")
-        antrian.append(nama)
-        print(f"{nama} ditambahkan ke antrian.")
-    elif pilih == "2":
-        if antrian:
-            dilayani = antrian.popleft()
-            print(f"{dilayani} telah dilayani.")
-        else:
-            print("Antrian kosong!")
-    elif pilih == "3":
-        print("Daftar antrian saat ini:")
-        for i, nama in enumerate(antrian, 1):
-            print(f"{i}. {nama}")
-    elif pilih == "4":
-        break
+#     if pilih == "1":
+#         nama = input("Masukkan nama pelanggan: ")
+#         antrian.append(nama)
+#         print(f"{nama} ditambahkan ke antrian.")
+#     elif pilih == "2":
+#         if antrian:
+#             dilayani = antrian.popleft()
+#             print(f"{dilayani} telah dilayani.")
+#         else:
+#             print("Antrian kosong!")
+#     elif pilih == "3":
+#         print("Daftar antrian saat ini:")
+#         for i, nama in enumerate(antrian, 1):
+#             print(f"{i}. {nama}")
+#     elif pilih == "4":
+#         break
+
+# Sistem daftar produk toko gym
+produk = {
+    "Dumbbell": 150000,
+    "Yoga Mat": 100000,
+    "Pull Up Bar": 250000,
+    "Protein Shake": 300000
+}
+
+print("=== DAFTAR PRODUK TOKO GYM ===")
+for nama, harga in produk.items():
+    print(f"{nama:15} : Rp. {harga}")
+
     
