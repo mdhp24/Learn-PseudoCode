7b4696e8ee0cd6a03cba3c24d76a717d9d626d0a
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2025-12-06 18:29:07 +0700

    Learn implements Python Day 43.5

--- test.py
+++ test.py
@@ -4061,9 +4061,22 @@
 #     print("Rating: Sangat Buruk ⭐")
 
 # Sistem cek tahun kabisat
-tahun = int(input("Masukkan tahun: "))
+# tahun = int(input("Masukkan tahun: "))
 
-if (tahun % 4 == 0 and tahun % 100 != 0) or (tahun % 400 == 0):
-    print("Tahun kabisat")
+# if (tahun % 4 == 0 and tahun % 100 != 0) or (tahun % 400 == 0):
+#     print("Tahun kabisat")
+# else:
+#     print("Bukan tahun kabisat")
+
+# Sistem tebak warna
+import random
+
+warna = ["merah", "biru", "hijau", "kuning"]
+jawaban = random.choice(warna)
+
+tebakan = input("Tebak warna (merah/biru/hijau/kuning): ")
+
+if tebakan == jawaban:
+    print("Benar!")
 else:
-    print("Bukan tahun kabisat")
+    print("Salah, warna yang benar adalah:", jawaban)
