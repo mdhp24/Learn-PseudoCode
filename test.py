477ac21f28881fbf988bf1e6ef08443087db2ac9
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2026-01-22 14:58:58 +0700

    Learn implements Python

--- test.py
+++ test.py
@@ -5875,8 +5875,17 @@
 
 
 # Sistem hitung stabilitas sesi pembelajaran berdasarkan waktu penyelesaian
-def session_stability(times):
-    if len(times) < 2:
-        return 0
-    return round(100 - abs(times[-1] - times[0]) / 2, 2)
-print(session_stability([300, 280, 290]))
+# def session_stability(times):
+#     if len(times) < 2:
+#         return 0
+#     return round(100 - abs(times[-1] - times[0]) / 2, 2)
+# print(session_stability([300, 280, 290]))
+
+
+# Sistem batasi jumlah hint yang ditampilkan
+def hint_limiter(hint_used):
+    if hint_used >= 3:
+        return "Hint Locked"
+    return "Hint Available"
+print(hint_limiter(4))
+
