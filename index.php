9c7f490d615c35905e26841f695fa35640026062
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2026-02-06 13:42:09 +0700

    Learn implements PHP .5

--- index.php
+++ index.php
@@ -47,15 +47,24 @@
 // echo $rule->evaluate(['attempts' => 5]); -->
 
 
-trait Logger {
-    public function log(string $message): void {
-        echo "[LOG] " . $message . PHP_EOL;
-    }
-}
+// trait Logger {
+//     public function log(string $message): void {
+//         echo "[LOG] " . $message . PHP_EOL;
+//     }
+// }
 
-class Chatbot {
-    use Logger;
-}
+// class Chatbot {
+//     use Logger;
+// }
 
-$bot = new Chatbot();
-$bot->log("User is struggling on Array topic");
+// $bot = new Chatbot();
+// $bot->log("User is struggling on Array topic");
+
+
+$scores = [60, 70, 80, 50, 90];
+
+$average = array_sum($scores) / count($scores);
+
+$status = $average < 70 ? "Low Performance" : "High Performance";
+
+echo "Average: $average - Status: $status";
