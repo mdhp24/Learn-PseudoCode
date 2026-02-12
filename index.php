c6fad35a04c948cf176cfde134dff3d445bd7b2f
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2026-02-12 11:26:01 +0700

    Learn implements PHP .6

--- index.php
+++ index.php
@@ -704,3 +704,23 @@ $dispatcher->listen("student.struggling", function($data) {
 });
 
 $dispatcher->dispatch("student.struggling", ['name' => 'Dicky']);
+
+class Database {
+
+    private static ?PDO $instance = null;
+
+    public static function connect(): PDO {
+        if (self::$instance === null) {
+            self::$instance = new PDO(
+                "mysql:host=localhost;dbname=pseudolearn",
+                "root",
+                ""
+            );
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        return self::$instance;
+    }
+}
+
+$db = Database::connect();
+echo "Database connected.";
