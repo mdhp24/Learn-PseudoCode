7c01cd24a3c1af0a7361452e5f04c5174842ba4e
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2026-03-23 14:03:50 +0700

    Update Mini Projek Pribadi Manajemen GYM

--- projek-gym/sistem-gym/resources/views/welcome.blade.php
+++ projek-gym/sistem-gym/resources/views/welcome.blade.php
@@ -751,6 +751,18 @@
                             <p class="text-xs text-dark-500">Trainer Profesional</p>
                         </div>
                     </a>
+                    <a href="{{ route('login') }}" class="flex items-center gap-3 group cursor-pointer">
+                        <div class="flex items-center gap-3 group cursor-pointer">
+                            <div
+                                class="w-12 h-12 bg-gradient-to-br from-primary-500 to-blue-600 rounded-xl flex items-center justify-center shadow-lg group-hover:scale-110 transition-transform duration-300">
+                                <i class="fas fa-comments text-white text-lg"></i>
+                            </div>
+                            <div class="text-left">
+                                <p class="text-white font-bold">Konsultasi</p>
+                                <p class="text-xs text-dark-500">Program Personal</p>
+                            </div>
+                        </div>
+                    </a>
 
                 </div>
             </div>
