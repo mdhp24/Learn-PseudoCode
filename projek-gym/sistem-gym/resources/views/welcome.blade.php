1c5efd8a8fc65500f0756f346ee934ffe4604ffa
Mochammad Dicky Hanun Prasetyo <dickyhanunprasetyo@gmail.com>
2026-03-28 12:52:01 +0700

    Update Mini Projek Pribadi Manajemen GYM

--- projek-gym/sistem-gym/resources/views/welcome.blade.php
+++ projek-gym/sistem-gym/resources/views/welcome.blade.php
@@ -1034,10 +1034,11 @@
                         <a href="#home" class="hover:text-primary-400 transition-colors">Terms of Service</a>
                         <span class="w-1 h-1 bg-dark-700 rounded-full"></span>
                         <a href="#home" class="hover:text-primary-400 transition-colors">Cookie Policy</a>
-                                </div>
+                    </div>
                 </div>
             </div>
         </div>
+        {{-- Decorative Top Border --}}
 
 </body>
 
